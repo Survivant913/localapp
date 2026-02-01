d9d14f1e42dd6caa33448a0a7bae72bc32f27bd1
Henni <hennimohammed.tourisme@gmail.com.com>
2026-02-01 12:42:06 +0100

    fesearabictest

--- src/Login.jsx
+++ src/Login.jsx
@@ -38,7 +38,7 @@ export default function Login() {
             {/* En-tête Épuré */}
             <div className="text-center mb-10 mt-2">
                 <h1 className="text-3xl font-bold text-white tracking-tight drop-shadow-sm">
-                    مرحباً
+                    أهلاً بك في مساحتك الشخصية
                 </h1>
             </div>
 
